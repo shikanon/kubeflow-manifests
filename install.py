8029a73d33f14192c7a34d3f1b7547d108148b67
shikanon <shikanon@users.noreply.github.com>
2021-05-21 11:44:02 +0800

    修改安装脚本

--- install.py
+++ install.py
@@ -16,6 +16,22 @@ def install(path):
             print(out)
             time.sleep(1)
 
+'''
+因为一些patch安装涉及到的一些修改需要重启pod，所以先删除再安装
+'''
+def patchInstall(path):
+    print("start to patch...")
+    for root,path,files in os.walk(path):
+        for f in files:
+            installfile = root + "/" + f
+            cmd_delete = "kubectl delete -f {installfile}".format(installfile=installfile)
+            p = subprocess.Popen(cmd_delete,shell=True,stdout=subprocess.PIPE)
+            out = p.stdout.read()
+            print(out)
+            cmd_apply = "kubectl apply -f {installfile}".format(installfile=installfile)
+            p = subprocess.Popen(cmd_apply,shell=True,stdout=subprocess.PIPE)
+            out = p.stdout.read()
+            print(out)
 
 # 安装文件
 path = "./manifest1.3"
@@ -23,4 +39,4 @@ install(path)
 
 # 安装patch
 patchPath = "./patch"
-install(patchPath)
+patchInstall(patchPath)
